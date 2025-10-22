b2ec240d53e13c1112c7f882adda2a537eeb7c20
ArsenKazaryanJS <arsen.kharyan@mail.ru>
2025-10-22 20:37:36 +0400

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/instagram__Story">
       <App />
     </BrowserRouter>
   </Provider>
